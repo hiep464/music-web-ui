ee12109fb998b411c515e9fd2d9c23af7aea4cb6
hiep464 <luuhiep2309@gmail.com>
2023-12-30 12:04:45 +0700

    first step

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,2 +1,5 @@
-export const baseApi = 'https://api.chientuan.store/api';
-export const rootBackend = 'https://api.chientuan.store';
+// export const baseApi = 'https://api.chientuan.store/api';
+// export const rootBackend = 'https://api.chientuan.store';
+
+export const baseApi = 'http://127.0.0.1:8000/api';
+export const rootBackend = 'http://127.0.0.1:8000';
